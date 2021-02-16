cf21019269f66969468740eda15a324ee768f6d4
alexrem1 <remidy@live.co.uk>
2021-02-16 17:04:00 +0000

    Do While Loops

---
+++ Novice to Ninja/Control-Flow-Basics/doWhileLoops.js
@@ -0,0 +1,9 @@
+// Sometimes you want the code to run once, even if the condition is not true
+// Do While Loop is an extension of the While Loop
+
+let i = 5;
+// do this code and it's going to do it once to begin with, regardless of the "condition: i < 5" being true or false
+do {
+  console.log("val of i is: ", i);
+  i++;
+} while (i < 5);
