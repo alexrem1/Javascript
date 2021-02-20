830804966d19162d3b65affe3d1485e79e0a0d38
alexrem1 <remidy@live.co.uk>
2021-02-20 00:56:49 +0000

    Query Selector

---
+++ Novice to Ninja/The-DOM/querySelector.js
@@ -0,0 +1,16 @@
+const para = document.querySelector("p");
+const paraError = document.querySelector(".error");
+const divError = document.querySelector("div.error");
+const headingOne = document.querySelector("body > h1");
+const paraTwo = document.querySelector("p:nth-child(2)");
+
+console.log(para, paraTwo, paraError, divError, headingOne);
+
+const paraAll = document.querySelectorAll("p");
+paraAll.forEach((paragraph) => {
+  console.log(paragraph);
+});
+console.log(paraAll[0]); // node list
+
+const errors = document.querySelectorAll(".error");
+console.log(errors);
