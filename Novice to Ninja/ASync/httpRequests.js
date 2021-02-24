24971a7e4fec8c380a27d51e44b2759c93121146
alexrem1 <remidy@live.co.uk>
2021-02-24 17:37:58 +0000

    HTTP request

---
+++ Novice to Ninja/ASync/httpRequests.js
@@ -0,0 +1,7 @@
+// make HTTP requests to get data from another server - we make these requests to API endpoints
+
+// So for example imagine we had a song library at API like Spotify or soundcloud it might have an endpoint which looks something like this "http://www.musicapi.com/artist/moby". So making a request to this end point would return us a list of mobi songs.
+
+// So from our Code which runs in the browser we would make a request to a server endpoint.Now that server would look at this request and say OK you want this data so I'll send it back to you as a response and then we'd get that data in our code and we can do something with it like render it to the browser if we want it.
+
+// once we make a request to an endpoint from the browser we typically get back a selection of data in a format called Json and Json is a format which looks very much like JavaScript objects and then we can do something with that data.
