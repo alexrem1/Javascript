0d5ba4b9923654af0e8a507dbc9217430e198d96
alexrem1 <remidy@live.co.uk>
2021-02-27 22:39:06 +0000

    Weather icons and images

--- Novice to Ninja/Projects/Weather-app/scripts/app.js
+++ Novice to Ninja/Projects/Weather-app/scripts/app.js
@@ -2,6 +2,8 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
   // const cityDetails = data.cityDetails;
@@ -15,8 +17,22 @@ const updateUI = (data) => {
           <h5 class="my-3">${cityDetails.EnglishName}</h5>
           <div class="my-3">${weather.WeatherText}</div>
           <div class="display-4 my-4">
-            <span>${weather.Temperature.Metric.Value}</span> <span>&deg;C</span>
+            <span>${weather.Temperature.Metric.Value}</span> 
+            <span>&deg;C</span>
           </div>`;
+
+  // update the night/day images & icon images
+  const iconSrc = `Images/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "Images/day.svg";
+  } else {
+    timeSrc = "Images/night.svg";
+  }
+  time.setAttribute("src", timeSrc);
+
   // remove the d-none class if present
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
