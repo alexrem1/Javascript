8c286579418e83e90649181a21e7dda9f7497e8c
alexrem1 <remidy@live.co.uk>
2021-03-01 17:07:21 +0000

    Storing & getting data

---
+++ Novice to Ninja/Local_storage/storingGettingData.js
@@ -0,0 +1,19 @@
+// store data in local store
+// 2 arguments: name of the key and the value of that key
+localStorage.setItem("name", "mario");
+localStorage.setItem("age", 50);
+
+// get data from local storage
+let name = localStorage.getItem("name");
+let age = localStorage.getItem("age");
+
+console.log(name, age);
+
+// updating data
+localStorage.setItem("name", "luigi");
+name = localStorage.getItem("name");
+
+localStorage.age = "100";
+age = localStorage.getItem("age");
+
+console.log(name, age);
