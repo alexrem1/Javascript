ec81caad39bba521666bf07e28b0e5c145ba0f1c
alexrem1 <remidy@live.co.uk>
2021-03-02 21:28:54 +0000

    NoSQL database

---
+++ Novice to Ninja/Databases_Firebase/nosqlDatabases.js
@@ -0,0 +1,5 @@
+// websites work with data eg blog posts, todos, comments etc
+// we can store this data in a database eg firebase
+
+// sql = tables, rows, columns
+// noSQL (database instance) = collections eg todos, blogs, pizzas, documents within these collections that look like JS objects, properties eg several key value pairs on the documents eg mongodb.
