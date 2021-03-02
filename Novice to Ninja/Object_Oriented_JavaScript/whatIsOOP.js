b3a3aa7b6df01b0b0c571781dd86e6fa6aac6aa6
alexrem1 <remidy@live.co.uk>
2021-03-02 14:23:23 +0000

    What is OOP?

---
+++ Novice to Ninja/Object_Oriented_JavaScript/whatIsOOP.js
@@ -0,0 +1,6 @@
+// Objects : properties and methods
+// eg const name = ["shaun", "alex"]
+// This contains an object array with a property length along with several methods
+
+// use contructor to create a wrapper object. It will contain a primitive value that has all the different properties and methods
+// eg const nameTwo = new String('Ryu');
