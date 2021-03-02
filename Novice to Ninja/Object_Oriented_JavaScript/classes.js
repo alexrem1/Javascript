ccd0be302f5d26e80675ff77f203182ec6cdf0cc
alexrem1 <remidy@live.co.uk>
2021-03-02 14:35:20 +0000

    Classes

---
+++ Novice to Ninja/Object_Oriented_JavaScript/classes.js
@@ -0,0 +1,9 @@
+// a class is a blueprint for an object (it describes how one should be made)
+// eg car blueprint has properties like its colour, model and functionality like ability to drive and reverse
+// we can specify the value of the properties, eg a new white/yellow/green car, with the same functionality
+
+// same principle for JS, class describes the type of object eg what properties and methods an object should have
+// userClass = {username, email, login(), logout()}
+// every user created with the "userClass" would have these properties and these methods but it can still be unique eg specification
+// eg new userClass("ken", "ken@ken.com")
+// eg new userClass("donkey", "donkey@donk.com")
