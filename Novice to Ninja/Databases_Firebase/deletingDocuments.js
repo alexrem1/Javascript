6332ee4fccc314aabd859eae7eb44b9ff8f81df2
alexrem1 <remidy@live.co.uk>
2021-03-04 23:11:01 +0000

    Deleting documents

---
+++ Novice to Ninja/Databases_Firebase/deletingDocuments.js
@@ -0,0 +1,63 @@
+const list = document.querySelector("ul");
+const form = document.querySelector("form");
+
+const addRecipe = (recipe, id) => {
+  let time = recipe.created_at.toDate();
+  let html = `
+        <li data-id="${id}">
+            <div>${recipe.title}</div>
+            <div>${time}</div>
+            <button class="btn btn-danger btn-sm my-2">delete</button>
+        </li>
+    `;
+  list.innerHTML += html;
+};
+
+// get documents
+db.collection("recipes")
+  .get()
+  .then((snapshot) => {
+    // when we have the data (response)
+    snapshot.docs.forEach((doc) => {
+      //   console.log(doc.data());
+      addRecipe(doc.data(), doc.id);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// add documents
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const now = new Date();
+  const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now), // firestore timestamp is a special object
+  };
+
+  db.collection("recipes")
+    .add(recipe)
+    .then(() => {
+      console.log("recipe added");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  form.reset();
+});
+
+// deleting documents
+list.addEventListener("click", (e) => {
+  //   console.log(e);
+  if (e.target.tagName === "BUTTON") {
+    const id = e.target.parentElement.getAttribute("data-id");
+    db.collection("recipes")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("recipe deleted");
+      });
+  }
+});
