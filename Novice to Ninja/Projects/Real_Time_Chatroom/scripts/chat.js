62ee738ab9b0d1efe2f32a388ba24212b5eb4834
alexrem1 <remidy@live.co.uk>
2021-03-08 17:29:31 +0000

    Chatroom project - Set up a real-time listener

--- Novice to Ninja/Projects/Real_Time_Chatroom/scripts/chat.js
+++ Novice to Ninja/Projects/Real_Time_Chatroom/scripts/chat.js
@@ -23,14 +23,27 @@ class Chatroom {
     const response = await this.chats.add(chat).then;
     return response;
   }
+
+  // real time listener - everytime there's a snapshot, it'll fire the first callback function to check if its a added type which will invoke the callback function passing in that data which gets fired on line 41
+  // calling line 41 once sets up this real time listener eg data added like new chats can be retreived and modified inside the callback function below
+  getChats(callback) {
+    this.chats.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          // update the ui
+          callback(change.doc.data());
+        }
+      });
+    });
+  }
 }
 
 const chatroom = new Chatroom("gaming", "shaun");
-chatroom
-  .addChat("hello everyone")
-  .then(() => console.log("chat added"))
-  .catch((err) => console.log(err));
-
+chatroom.getChats((data) => {
+  console.log(data);
+  // call the update function on the ui class
+});
 // step 1: create a class
 // step 2: create a new "Chatroom"
 // step 3: create an async method to add new chat documents
+// step 4: set up a real-time listener
