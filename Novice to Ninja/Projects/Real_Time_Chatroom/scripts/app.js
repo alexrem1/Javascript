7a8e90cdb2ad45ab08da5a835695ca741839ed14
alexrem1 <remidy@live.co.uk>
2021-03-08 22:25:36 +0000

    Chatroom project - changing username and local storage

--- Novice to Ninja/Projects/Real_Time_Chatroom/scripts/app.js
+++ Novice to Ninja/Projects/Real_Time_Chatroom/scripts/app.js
@@ -3,6 +3,8 @@
 // DOM queries
 const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
+const newNameForm = document.querySelector(".new-name");
+const updateMssg = document.querySelector(".update-mssg");
 
 // add a new chat
 newChatForm.addEventListener("submit", (e) => {
@@ -16,9 +18,26 @@ newChatForm.addEventListener("submit", (e) => {
     .catch((err) => console.log(err)); // does not matter class instance is below because by the time a user actually submits a form eg type something into the field and submits it all of the javascript has already run. So chat room already exists at this point.
 });
 
+// update username
+newNameForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // update name via the chatroom class
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  newNameForm.reset();
+  //show then hide the update message
+  updateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => {
+    updateMssg.innerText = "";
+  }, 3000);
+});
+
+// check local storage for a name
+const username = localStorage.username ? localStorage.username : "anonymous";
+
 // class instance
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom("general", "shaun");
+const chatroom = new Chatroom("general", username);
 
 // ultimately we want to be able to render the chat data to the DOM and to do that we'll create a another class called Chat UI inside the UI.js
 chatroom.getChats((data) => chatUI.render(data));
