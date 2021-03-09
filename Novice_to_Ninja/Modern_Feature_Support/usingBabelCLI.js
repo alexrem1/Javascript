ec4e834da71ea5063975ff0da00dc85145f8d5c5
alexrem1 <remidy@live.co.uk>
2021-03-09 16:56:33 +0000

    NPM scripts and watching files

--- Novice_to_Ninja/Modern_Feature_Support/usingBabelCLI.js
+++ Novice_to_Ninja/Modern_Feature_Support/usingBabelCLI.js
@@ -7,3 +7,7 @@ const greet = (name) => {
 greet();
 
 // convert via node_modules/.bin/babel Modern_Feature_Support/usingBabelCLI.js -o Modern_Feature_Support/usingBabelCLIResult.js
+// OR
+// DEFINE A SCRIPT INSIDE PACKAGE.JSON
+// OR
+// use a special watch flag eg -w to run script automatically
