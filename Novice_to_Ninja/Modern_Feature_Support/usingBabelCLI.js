6670fc523b8aafaf64ab416e8072e9eb0db3d43f
alexrem1 <remidy@live.co.uk>
2021-03-09 16:38:52 +0000

    Using the babel CLI

---
+++ Novice_to_Ninja/Modern_Feature_Support/usingBabelCLI.js
@@ -0,0 +1,9 @@
+// before conversion
+
+const greet = (name) => {
+  console.log(`hello ${name}`);
+};
+
+greet();
+
+// convert via node_modules/.bin/babel Modern_Feature_Support/usingBabelCLI.js -o Modern_Feature_Support/usingBabelCLIResult.js
---
+++ Novice_to_Ninja/Modern_Feature_Support/usingBabelCLIResult.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// after conversion - fully compatible with all browsers
+var greet = function greet(name) {
+  console.log("hello ".concat(name));
+};
+
+greet();
