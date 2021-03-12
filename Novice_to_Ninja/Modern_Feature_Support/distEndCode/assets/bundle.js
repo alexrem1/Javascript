9620262d12bcad2c27590a7a94598360ea967d10
alexrem1 <remidy@live.co.uk>
2021-03-12 14:25:42 +0000

    Webpack CLI

--- Novice_to_Ninja/Modern_Feature_Support/distEndCode/assets/bundle.js
+++ Novice_to_Ninja/Modern_Feature_Support/distEndCode/assets/bundle.js
@@ -1,17 +1,1 @@
-"use strict";
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var greet = function greet(name) {
-  console.log("hello ".concat(name));
-};
-
-greet("mario");
-greet("luigi");
-greet("link");
-
-var User = function User() {
-  _classCallCheck(this, User);
-
-  this.score = 0;
-};
+(()=>{const o=o=>{console.log(`hello ${o}`)};o("mario"),o("luigi"),o("link"),o("looneyToon")})();
