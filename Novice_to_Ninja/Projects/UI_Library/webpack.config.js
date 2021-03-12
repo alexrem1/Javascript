3eb83ee5637361ae4c00c763a7203974ba7c2133
alexrem1 <remidy@live.co.uk>
2021-03-12 18:23:09 +0000

    Ui Library - Project setup

---
+++ Novice_to_Ninja/Projects/UI_Library/webpack.config.js
@@ -0,0 +1,31 @@
+// module.exports {} represents options and configurations for how webpack is going to run
+// only available when running javascript on a computer using Node.js
+// __dirname gets the complete absolute path up until this directory eg the root directory of the project where the web file is sitting
+
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/assets"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    publicPath: "/assets/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
